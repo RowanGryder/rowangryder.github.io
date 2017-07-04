7b0bf4b88396d171e5c726704a6051c4d2c8e878
RowanGryder <rowan.gryder@gmail.com>
2017-07-04 00:25:27 -0400

    Album Structures
    Completed album structures and linking of images

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,15 @@
+// Code by Rowan Guarnagia-Gryder
+
 function navFold(x) {
     x.classList.toggle("change");
-    
-//    var lm = document.getElementsByClassName("mainLinks");
-//    for (var i = 0; i > lm.length; i++){
-//        var r = document.getElementsByClassName("mainLinks")[i];
-//        r.classList.toggle("change");
-//    }
-//    lm.classList.toggle("change")
     
     var lm = document.getElementById("elem");
     lm.classList.toggle("change");
 }
 
-//function responsiveNav() {
-//    var x = document.getElementById("navBar");
-//    if(x.className === "navBar"){
-//        x.className += " responsive";
-//    } else {
-//        x.className = "navBar";
-//    }
-//}
+function scrollTarget() {
+    var target = document.getElementById("target");
+    var source = document.getElementById("source");
+    target.scrollTop = source.scrollTop;
+    target.scrollLeft = source.scrollLeft;
+}
