8221d2027baf444a1392351191c5f0742ed72752
RowanGryder <rowan.gryder@gmail.com>
2017-07-06 13:01:59 -0400

    Added Indexes

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,27 @@
 function navFold(x) {
     x.classList.toggle("change");
     
-    var lm = document.getElementById("elem");
-    lm.classList.toggle("change");
+//    var lm = document.getElementById("elem");
+//    lm.classList.toggle("change");
+    
+    document.getElementById("elem").classList.toggle("change");
+}
+
+function indexFold() {
+    
+}
+
+function indexOpen() {
+    document.getElementById("indexBar").style.transform = "translate(0%)";
+//    document.getElementById("indexBar").classList.add("open");
+
+    document.getElementById("indexSel").style.display = "none";
+}
+
+function indexClose() {
+    document.getElementById("indexBar").style.transform = "translate(100%)";
+//    document.getElementById("indexBar").classList.remove("");
+    document.getElementById("indexSel").style.display = "block";
 }
 
 function scrollTarget() {
